8dd7ae576a29b72767c16048cb29ec329f5b2f35
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-17 12:13:21 +0530

    Book a table app bar

---
+++ lib/view/book_a_table/widget/book_a_table_appbar.dart
@@ -0,0 +1,45 @@
+import 'package:booking_table/utils/common/common_colors.dart';
+import 'package:booking_table/utils/common/common_strings.dart';
+import 'package:booking_table/utils/common/widgets_methods/common_sized_box.dart';
+import 'package:flutter/material.dart';
+
+AppBar bookATableAppBar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    title: const Text(
+      'Book a Table',
+      style: TextStyle(
+          fontSize: 22,
+          color: black000000,
+          fontWeight: FontWeight.w600,
+          fontFamily: mainLatoFont),
+    ),
+    elevation: 0,
+    leadingWidth: 80,
+    leading: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CommonSizedBox(
+          width: 5,
+        ),
+        const Icon(
+          size: 20,
+          Icons.arrow_back,
+          color: black000000,
+        ),
+        CommonSizedBox(
+          width: 5,
+        ),
+        const Text(
+          'Back',
+          style: TextStyle(
+              fontSize: 18,
+              color: black000000,
+              fontWeight: FontWeight.w400,
+              fontFamily: mainLatoFont),
+        ),
+      ],
+    ),
+  );
+}
