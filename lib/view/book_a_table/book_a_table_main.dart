0113afac096f08b3124e253d0908f271ae345be1
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-17 12:13:43 +0530

    Book a table main

--- lib/view/book_a_table/book_a_table_main.dart
+++ lib/view/book_a_table/book_a_table_main.dart
@@ -1,3 +1,5 @@
+import 'package:booking_table/view/book_a_table/book_a_table_body.dart';
+import 'package:booking_table/view/book_a_table/widget/book_a_table_appbar.dart';
 import 'package:flutter/material.dart';
 
 class BookATableView extends StatelessWidget {
@@ -5,6 +7,12 @@ class BookATableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: bookATableAppBar(),
+      body: SingleChildScrollView(
+        child: BookATableBody(),
+      ),
+    );
   }
 }
