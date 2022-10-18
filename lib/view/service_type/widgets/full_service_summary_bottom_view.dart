45f5d5398ef6979e2524df51629f9e3e964b56b3
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-18 13:31:00 +0530

    Minor Changes

--- lib/view/service_type/widgets/full_service_summary_bottom_view.dart
+++ lib/view/service_type/widgets/full_service_summary_bottom_view.dart
@@ -1,4 +1,4 @@
-import 'package:booking_table/controller/summary/summary_controller.dart';
+import 'package:booking_table/controller/service_type/summary_controller.dart';
 import 'package:booking_table/utils/common/common_colors.dart';
 import 'package:booking_table/utils/common/images_string.dart';
 import 'package:booking_table/utils/common/widgets_methods/common_button.dart';
