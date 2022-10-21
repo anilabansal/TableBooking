f459c0e6113335dac0fc71dcf5f56dc44ea31525
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-21 12:39:40 +0530

    Minor Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:booking_table/view/notifications/notification_view.dart';
+import 'package:booking_table/view/splash_screen/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       title: 'Flutter Demo',
       home: Material(
-        child: NotificationView(),
+        child: SplashView(),
       ),
     );
   }
--- lib/view/notifications/notification_view.dart
+++ lib/view/notifications/notification_view.dart
@@ -3,7 +3,7 @@ import 'package:booking_table/utils/common/common_colors.dart';
 import 'package:booking_table/utils/common/widgets_methods/common_app_bar.dart';
 import 'package:booking_table/utils/common/widgets_methods/common_sized_box.dart';
 import 'package:booking_table/utils/common/widgets_methods/common_text.dart';
-import 'package:flutter/gestures.dart';
+import 'package:booking_table/view/notifications/widgets/notification_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:get/get.dart';
@@ -39,7 +39,8 @@ class NotificationView extends StatelessWidget {
                         width: 31.0,
                         height: 16.0,
                         valueFontSize: 0.0,
-                        toggleSize: 10.0, inactiveColor: Color(0xFFD9D9D9),
+                        toggleSize: 10.0,
+                        inactiveColor: const Color(0xFFD9D9D9),
 
                         value: controller.isOpen.value,
                         borderRadius: 00.0,
@@ -94,101 +95,6 @@ class NotificationView extends StatelessWidget {
             const NotificationWidget(),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class NotificationWidget extends StatelessWidget {
-  const NotificationWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return CommonSizedBox(
-      height: 120,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          CommonText(
-            fontSize: 14,
-            text: '22 July, 2022',
-            textAlign: TextAlign.start,
-            fontWeight: FontWeight.w400,
-          ),
-          CommonSizedBox(
-            height: 15,
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 48,
-                  alignment: Alignment.topCenter,
-                  width: 48,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFCE9E9),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      Icons.paste,
-                      color: redE2211C,
-                    ),
-                  ),
-                ),
-                CommonSizedBox(
-                  width: 15,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            const TextSpan(
-                                text: '@Josh Cafe ',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    color: redE2211C,
-                                    fontSize: 16)),
-                            TextSpan(
-                              text: 'confirmed your booking for 23 july, 2022',
-                              style: const TextStyle(
-                                color: black040404,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  // TODO : TERMS & CONDITIONS
-                                },
-                            ),
-                          ],
-                        ),
-                      ),
-                      CommonSizedBox(
-                        height: 6,
-                      ),
-                      CommonText(
-                        fontSize: 12,
-                        text: '3 Min Ago',
-                        fontWeight: FontWeight.w400,
-                      ),
-                      CommonSizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
