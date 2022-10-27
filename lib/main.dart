20089f305d96f3b445e7321888dca178b1a77d5c
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-27 18:23:33 +0530

    Routes Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:booking_table/view/splash_screen/splash_view.dart';
+import 'package:booking_table/view/service_type/full_service_summary.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,9 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       home: Material(
-        child: SplashView(),
+        child: FullServiceSummaryView(
+          callFrom: '',
+        ),
       ),
     );
   }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,68 +1,120 @@
-import 'package:booking_table/routes/route_name.dart';
-import 'package:booking_table/view/auth_screens/get_zip_code_view.dart';
-import 'package:booking_table/view/auth_screens/otp_screen.dart';
-import 'package:booking_table/view/auth_screens/sign_in_view.dart';
-import 'package:booking_table/view/book_a_table/book_a_table_main.dart';
-import 'package:booking_table/view/favourite/favourite_view.dart';
-import 'package:booking_table/view/home_screen/home_view.dart';
-import 'package:booking_table/view/notifications/notification_view.dart';
-import 'package:booking_table/view/payment/add_card_details.dart';
-import 'package:booking_table/view/payment/payment_done.dart';
-import 'package:booking_table/view/privacy_policy/privacy_policy.dart';
-import 'package:booking_table/view/rate/rate_review.dart';
-import 'package:booking_table/view/reservation/reservation_view.dart';
-import 'package:booking_table/view/reservation/widgets/previous_reservation.dart';
-import 'package:booking_table/view/reservation/widgets/upcoming_reservation.dart';
-import 'package:booking_table/view/service_type/pre_order.dart';
-import 'package:booking_table/view/service_type/widgets/full_service_summary_details_button_view.dart';
-import 'package:booking_table/view/service_type/widgets/mid_service_summary_details_button_view.dart';
-import 'package:booking_table/view/splash_screen/splash_view.dart';
-import 'package:booking_table/view/support/support_view.dart';
-import 'package:booking_table/view/terms_n_conditions/terms_n_conditions.dart';
 import 'package:get/get.dart';
 
+import '/routes/route_name.dart';
+import '/view/auth_screens/get_zip_code_view.dart';
+import '/view/auth_screens/otp_screen.dart';
+import '/view/auth_screens/sign_in_view.dart';
+import '/view/book_a_table/book_a_table_main.dart';
+import '/view/favourite/favourite_view.dart';
+import '/view/home_screen/home_view.dart';
+import '/view/notifications/notification_view.dart';
+import '/view/payment/add_card_details.dart';
+import '/view/payment/payment_done.dart';
+import '/view/privacy_policy/privacy_policy.dart';
+import '/view/rate/rate_review.dart';
+import '/view/reservation/reservation_view.dart';
+import '/view/reservation/widgets/previous_reservation.dart';
+import '/view/reservation/widgets/upcoming_reservation.dart';
+import '/view/service_type/pre_order.dart';
+import '/view/service_type/widgets/full_service_summary_details_button_view.dart';
+import '/view/service_type/widgets/mid_service_summary_details_button_view.dart';
+import '/view/splash_screen/splash_view.dart';
+import '/view/support/support_view.dart';
+import '/view/terms_n_conditions/terms_n_conditions.dart';
+
 appRoutes() => [
-      GetPage(name: RouteName.home, page: () => const HomeView()),
-      GetPage(name: RouteName.splash, page: () => const SplashView()),
       GetPage(
-        name: RouteName.login,
-        page: () => SignInView(
-          callFrom: 'Login',
-        ),
+        name: RouteName.home, page: () => const HomeView(),
+        // binding: ,
       ),
       GetPage(
-          name: RouteName.register,
-          page: () => SignInView(callFrom: 'Register')),
+        name: RouteName.splash, page: () => const SplashView(),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.loginOTP,
-          page: () => OtpScreenView(callFrom: 'Login')),
+        name: RouteName.login, page: () => SignInView(callFrom: 'Login'),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.registerOTP,
-          page: () => OtpScreenView(callFrom: 'Register')),
-      GetPage(name: RouteName.support, page: () => SupportView()),
-      GetPage(name: RouteName.favourite, page: () => FavouriteView()),
-      GetPage(name: RouteName.notifications, page: () => NotificationView()),
+        name: RouteName.register, page: () => SignInView(callFrom: 'Register'),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.addCardDetails,
-          page: () => AddCreditCardDetailsView()),
-      GetPage(name: RouteName.paymentDone, page: () => PaymentDoneView()),
-      GetPage(name: RouteName.privacyPolicy, page: () => PrivacyPolicyView()),
+        name: RouteName.loginOTP, page: () => OtpScreenView(callFrom: 'Login'),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.termsNConditions, page: () => TermsNConditionsView()),
-      GetPage(name: RouteName.reviews, page: () => RateReviewView()),
-      GetPage(name: RouteName.paymentDone, page: () => PaymentDoneView()),
-      GetPage(name: RouteName.reservation, page: () => ReservationView()),
+        name: RouteName.registerOTP,
+        page: () => OtpScreenView(callFrom: 'Register'),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.fullService,
-          page: () => FullSummaryBookingDetailsView()),
+        name: RouteName.support, page: () => SupportView(),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.midService,
-          page: () => MidSummaryBookingDetailsView()),
-      GetPage(name: RouteName.preOrder, page: () => PreOrderView()),
+        name: RouteName.favourite, page: () => FavouriteView(),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.previousBooking, page: () => PreviousReservations()),
+        name: RouteName.notifications, page: () => NotificationView(),
+        // binding: ,
+      ),
       GetPage(
-          name: RouteName.upcomingBooking, page: () => UpComingReservations()),
-      GetPage(name: RouteName.bookATable, page: () => BookATableView()),
-      GetPage(name: RouteName.zipCodeView, page: () => GetZipCodeView()),
+        name: RouteName.addCardDetails, page: () => AddCreditCardDetailsView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.paymentDone, page: () => PaymentDoneView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.privacyPolicy, page: () => PrivacyPolicyView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.termsNConditions, page: () => TermsNConditionsView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.reviews, page: () => RateReviewView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.paymentDone, page: () => PaymentDoneView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.reservation, page: () => ReservationView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.fullService,
+        page: () => FullSummaryBookingDetailsView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.midService, page: () => MidSummaryBookingDetailsView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.preOrder, page: () => PreOrderView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.previousBooking, page: () => PreviousReservations(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.upcomingBooking, page: () => UpComingReservations(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.bookATable, page: () => BookATableView(),
+        // binding: ,
+      ),
+      GetPage(
+        name: RouteName.zipCodeView, page: () => GetZipCodeView(),
+        // binding: ,
+      ),
     ];
