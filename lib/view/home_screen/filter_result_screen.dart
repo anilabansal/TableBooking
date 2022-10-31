1beb8356019aa027f10c4ad41d3d5b43ee88ad59
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-31 17:35:36 +0530

    Comment Common fixes

--- lib/view/home_screen/filter_result_screen.dart
+++ lib/view/home_screen/filter_result_screen.dart
@@ -1,13 +1,7 @@
-import 'package:booking_table/controller/home/home_controller.dart';
 import 'package:booking_table/utils/common/common_colors.dart';
-import 'package:booking_table/utils/common/widgets_methods/common_text.dart';
-import 'package:booking_table/view/home_screen/widgets/filter_restaurant_widget.dart';
-import 'package:booking_table/view/home_screen/widgets/map_home_widget.dart';
+import 'package:booking_table/view/home_screen/widgets/filter_result_body.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../../utils/common/common_font.dart';
-import '../../utils/common/images_string.dart';
-import '../../utils/common/widgets_methods/app_bar_back_button.dart';
+
 import '../../utils/common/widgets_methods/common_app_bar.dart';
 
 class FilterResultScreen extends StatelessWidget {
@@ -16,126 +10,8 @@ class FilterResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: white,
-      appBar: appBarCommon(
-        text: "Filter Result"
-      ),
-      body: SingleChildScrollView(
-        child: GetBuilder<HomeController>(builder: (homeController) {
-          return Column(
-            children: [
-              // Padding(
-              //   padding: const EdgeInsets.all(15.0),
-                // child: Stack(
-                //   children: [
-                //     const AppBarBackButton(),
-                //     Center(
-                //       child: CommonText(
-                //         text: "Filter Result",
-                //         fontSize: 22,
-                //         fontWeight: FontWeight.w600,
-                //         color: black000000,
-                //       ),
-                //     )
-                //   ],
-                // ),
-              //),
-              // const SizedBox(
-              //   height: 35,
-              // ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15.0, 20, 15, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CommonText(
-                      text: "1 Restaurants",
-                      fontFamily: interFont,
-                      fontWeight: FontWeight.w500,
-                      color: black000000,
-                      fontSize: 16,
-                    ),
-                    Container(
-                      width: 57,
-                      height: 29,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: greyF4F4F4),
-                      child: Center(
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(0.0),
-                              child: InkWell(
-                                onTap: () {
-                                  homeController.restaurantFilter.value =
-                                      true;
-                                  homeController.update();
-                                },
-                                child: Container(
-                                  width: 24,
-                                  height: 25,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(4),
-                                      color: homeController
-                                              .restaurantFilter.value
-                                          ? black000000
-                                          : greyF4F4F4),
-                                  child: Center(
-                                      child: Image.asset(
-                                    menuImage,
-                                    width: 14,
-                                    height: 14,
-                                    color:
-                                        homeController.restaurantFilter.value
-                                            ? white
-                                            : greyC1C1C1,
-                                  )),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            InkWell(
-                              onTap: () {
-                                homeController.restaurantFilter.value = false;
-                                homeController.update();
-                              },
-                              child: Container(
-                                width: 24,
-                                height: 25,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    color: homeController
-                                                .restaurantFilter.value ==
-                                            false
-                                        ? black000000
-                                        : greyF4F4F4),
-                                child: Center(
-                                    child: Image.asset(locationImage,
-                                        width: 14,
-                                        height: 14,
-                                        color: homeController
-                                                    .restaurantFilter.value ==
-                                                false
-                                            ? white
-                                            : greyC1C1C1)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              homeController.restaurantFilter.value
-                  ? const FilterRestaurantScreen()
-                  : const MapHomeScreen()
-            ],
-          );
-        }),
-      ),
+      appBar: appBarCommon(text: "Filter Result"),
+      body: FilterResultBody(),
     );
   }
 }
