1e8e85877a61f96a873223dfc53590caf525c099
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-31 17:26:13 +0530

    Comment Common fixes

---
+++ lib/view/auth_screens/widgets/auth_selection_body.dart
@@ -0,0 +1,48 @@
+import 'package:booking_table/utils/common/common_colors.dart';
+import 'package:booking_table/utils/common/widgets_methods/common_text.dart';
+import 'package:booking_table/view/auth_screens/widgets/auth_selection_view_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthSelectionBody extends StatelessWidget {
+  const AuthSelectionBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // TOP
+            SizedBox(height: Get.height * 0.15),
+            // Middle View
+            const AuthScreenViewWidget(),
+            // Bottom
+            Column(
+              children: [
+                // Button
+                InkWell(
+                  onTap: () {
+                    Get.toNamed('/home');
+                  },
+                  child: CommonText(
+                    text: 'Continue as Guest',
+                    color: redE2211C,
+                    decoration: TextDecoration.underline,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
