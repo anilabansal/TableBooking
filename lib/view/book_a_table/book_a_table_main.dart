bafde11539696e1f5d17eb7545cfee86dfb24386
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-31 17:26:24 +0530

    Comment Common fixes

--- lib/view/book_a_table/book_a_table_main.dart
+++ lib/view/book_a_table/book_a_table_main.dart
@@ -1,18 +1,17 @@
 import 'package:booking_table/utils/common/widgets_methods/common_app_bar.dart';
-import 'package:booking_table/view/book_a_table/book_a_table_body.dart';
+import 'package:booking_table/view/book_a_table/widget/book_a_table_body.dart';
 import 'package:flutter/material.dart';
 
 class BookATableView extends StatelessWidget {
-  const BookATableView({Key? key}) : super(key: key);
+  String callFrom;
+  BookATableView({Key? key, required this.callFrom}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: appBarCommon(text: 'Book a Table'),
-      body: SingleChildScrollView(
-        child: BookATableBody(),
-      ),
+      body: BookATableBody(),
     );
   }
 }
