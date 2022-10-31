1ade6fa7639d3b299ae277ec957fd0cf1891e4ce
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-31 17:34:16 +0530

    Comment Common fixes

--- lib/view/payment/card_scan_pop_up.dart
+++ lib/view/payment/card_scan_pop_up.dart
@@ -1,33 +1,33 @@
 import 'package:booking_table/controller/payment/card_scan_controller.dart';
-import 'package:booking_table/utils/common/images_string.dart';
-import 'package:credit_card_scanner/credit_card_scanner.dart';
+import 'package:booking_table/view/payment/widgets/card_scan_pop_up_body.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class CardScanPopUpView extends StatefulWidget {
-  const CardScanPopUpView({Key? key}) : super(key: key);
-  @override
-  State<CardScanPopUpView> createState() => _CardScanPopUpViewState();
-}
-class _CardScanPopUpViewState extends State<CardScanPopUpView> {
+class CardScanPopUpView extends StatelessWidget {
+  CardScanPopUpView({Key? key}) : super(key: key);
+  // @override
+  // State<CardScanPopUpView> createState() => _CardScanPopUpViewState();
+// }
+//
+// class _CardScanPopUpViewState extends State<CardScanPopUpView> {
   //CardDetails? _cardDetails;
-  CardScanOptions scanOptions = const CardScanOptions(
-    scanCardHolderName: true,
-    //  enableDebugLogs: true,
-    // validCardsToScanBeforeFinishingScan: 5,
-    possibleCardHolderNamePositions: [
-      CardHolderNameScanPosition.belowCardNumber,
-    ],
-  );
+  // CardScanOptions scanOptions = const CardScanOptions(
+  //   scanCardHolderName: true,
+  //   //  enableDebugLogs: true,
+  //   // validCardsToScanBeforeFinishingScan: 5,
+  //   possibleCardHolderNamePositions: [
+  //     CardHolderNameScanPosition.belowCardNumber,
+  //   ],
+  // );
 
-  scanCardDetails() async {
-    final CardDetails? cardDetails =
-        await CardScanner.scanCard(scanOptions: scanOptions);
-    if (!mounted || cardDetails == null) return;
-    // setState(() {
-    //   _cardDetails = cardDetails;
-    // });
-  }
+  // scanCardDetails() async {
+  //   final CardDetails? cardDetails =
+  //       await CardScanner.scanCard(scanOptions: scanOptions);
+  //   if (!mounted || cardDetails == null) return;
+  // setState(() {
+  //   _cardDetails = cardDetails;
+  // });
+  // }
 
   CardScanController controller = Get.put(CardScanController());
 
@@ -35,35 +35,7 @@ class _CardScanPopUpViewState extends State<CardScanPopUpView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          height: 280,
-          width: Get.width,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 36,
-          ),
-          child: InkWell(
-            onTap: () {
-              scanCardDetails();
-              // Get.to(()=>const ScanCardScreen());
-            },
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                    child: Image.asset(
-                  scanCard,
-                  height: 168,
-                )),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: CardScanPopUpBody(controller: controller),
     );
   }
 }
