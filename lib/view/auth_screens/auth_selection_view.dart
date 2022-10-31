b870d8657ce21491b3a7bc61c84b8f4928075db3
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-10-31 17:26:17 +0530

    Comment Common fixes

--- lib/view/auth_screens/auth_selection_view.dart
+++ lib/view/auth_screens/auth_selection_view.dart
@@ -1,48 +1,13 @@
-import 'package:booking_table/utils/common/widgets_methods/common_text.dart';
-import 'package:booking_table/view/auth_screens/widgets/auth_selection_view_widget.dart';
+import 'package:booking_table/view/auth_screens/widgets/auth_selection_body.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import '../../utils/common/common_colors.dart';
 
 class AuthSelectionView extends StatelessWidget {
   const AuthSelectionView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // TOP
-              SizedBox(height: Get.height * 0.15),
-              // Middle View
-              const AuthScreenViewWidget(),
-              // Bottom
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Get.toNamed('/home');
-                    },
-                    child: CommonText(
-                      text: 'Continue as Guest',
-                      color: redE2211C,
-                      decoration: TextDecoration.underline,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+    return const Scaffold(
+      body: AuthSelectionBody(),
     );
   }
 }
