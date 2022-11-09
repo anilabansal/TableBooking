3363c713e5109058c57cd92a896aa6926a82192f
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-11-09 15:59:05 +0530

    Add Multipart APi CALLs

--- lib/utils/network/api_calls.dart
+++ lib/utils/network/api_calls.dart
@@ -1,14 +1,19 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:get/get.dart';
 
 import '../common/common_strings.dart';
 
 class ApiCalls extends GetConnect {
-/**
-* This method is for get request to the server.
- **/
+  var imageFile = File('').obs;
+  var imageUrl = ''.obs;
 
+  updateImageFile(File value) {
+    imageFile.value = value;
+  }
+
+  /// This method is for get request to the server.
   Future<dynamic> callPostApi(Map<String, dynamic> body, String endPoint,
       {bool isToken = false,
       String token = '',
@@ -50,10 +55,51 @@ class ApiCalls extends GetConnect {
     return;
   }
 
-  /**
-  * This method is for get request
-  **/
+  /// This method is for get request with multipart to the server.
+  Future<dynamic> callPostApiWithFile(
+      Map<String, dynamic> body, String endPoint,
+      {bool isToken = false,
+      String token = '',
+      // bool isFullUrl = false,
+      // String baseUrl,
+      isPayment = false,
+      bool isString = false}) async {
+    Map<String, String> withToken;
 
+    withToken = {
+      "Content-Type": "multipart/form-data",
+      'accept': 'text/plain',
+      "AuthToken": token,
+    };
+    print(
+        'API Request Header ------------------------------->\n ${jsonEncode(withToken)}');
+    String url = '$baseURL/$endPoint';
+    print('URL Request ------------------------------->\n $url');
+    try {
+      var response = await post(
+        url,
+        body,
+        headers: withToken,
+      );
+      print(
+          'API response ------------------------------->\n ${response.statusCode}');
+
+      print('API response ------------------------------->\n ${response.body}');
+
+      print(
+          'API final body ------------------------------->\n ${response.body.toString()}');
+
+      print(
+          'API request Header ------------------------------->\n ${response.headers}');
+      print('Run Successfully!!!!!');
+      return response;
+    } catch (e) {
+      print("========> Responses Error ${e.toString()}");
+    }
+    return;
+  }
+
+  /// This method is for get request
   Future<dynamic> callGetApi(String endPoint, {String token = ''}) async {
     try {
       print('token ------------------>\n $token');
