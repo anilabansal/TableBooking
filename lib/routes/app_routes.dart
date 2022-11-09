ba39f3bade16aff9817ce0792540cd147c556a3d
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-11-09 15:58:49 +0530

    Sort Imports & Routes Name Update

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,32 +1,32 @@
 import 'package:booking_table/bindings/auth_bindings.dart';
 import 'package:booking_table/view/auth_screens/auth_selection_view.dart';
-import 'package:booking_table/view/book_a_table/booking_details.dart';
-import 'package:booking_table/view/home_screen/filter_screen.dart';
-import 'package:booking_table/view/home_screen/restaurant_details_screen.dart';
-import 'package:booking_table/view/profile_screen/profile_screen.dart';
-import 'package:booking_table/view/service_type/full_service_summary.dart';
-import 'package:booking_table/view/service_type/mid_service_summary.dart';
+import 'package:booking_table/view/book_a_table/booking_details_view.dart';
+import 'package:booking_table/view/home_screen/filter_view.dart';
+import 'package:booking_table/view/home_screen/restaurant_details_view.dart';
+import 'package:booking_table/view/profile_screen/profile_view.dart';
+import 'package:booking_table/view/service_type/full_service_summary_view.dart';
+import 'package:booking_table/view/service_type/mid_service_summary_view.dart';
 import 'package:get/get.dart';
 
 import '/routes/route_name.dart';
 import '/view/auth_screens/get_zip_code_view.dart';
-import '/view/auth_screens/otp_screen.dart';
+import '/view/auth_screens/otp_view.dart';
 import '/view/auth_screens/sign_in_view.dart';
-import '/view/book_a_table/book_a_table_main.dart';
+import '/view/book_a_table/book_a_table_view.dart';
 import '/view/favourite/favourite_view.dart';
 import '/view/home_screen/home_view.dart';
 import '/view/notifications/notification_view.dart';
-import '/view/payment/add_card_details.dart';
-import '/view/payment/payment_done.dart';
-import '/view/privacy_policy/privacy_policy.dart';
-import '/view/rate/rate_review.dart';
+import '/view/payment/add_card_details_view.dart';
+import '/view/payment/payment_done_view.dart';
+import '/view/privacy_policy/privacy_policy_view.dart';
+import '/view/rate/rate_review_view.dart';
 import '/view/reservation/reservation_view.dart';
-import '/view/service_type/pre_order.dart';
+import '/view/service_type/pre_order_view.dart';
 import '/view/splash_screen/splash_view.dart';
 import '/view/support/support_view.dart';
-import '/view/terms_n_conditions/terms_n_conditions.dart';
-import '../view/home_screen/filter_result_screen.dart';
-import '../view/payment/payment_method_screen.dart';
+import '/view/terms_n_conditions/terms_n_conditions_view.dart';
+import '../view/home_screen/filter_result_view.dart';
+import '../view/payment/payment_method_view.dart';
 
 class AppRoutes {
   static final appRoutes = [
@@ -40,7 +40,7 @@ class AppRoutes {
     ),
     GetPage(
       name: RouteName.createProfile,
-      page: () => EditProfileScreen(callFrom: 'Create Profile'),
+      page: () => ProfileView(callFrom: 'Create Profile'),
       // binding: ,
     ),
     GetPage(
@@ -60,12 +60,12 @@ class AppRoutes {
     ),
     GetPage(
       name: RouteName.loginOTP,
-      page: () => OtpScreenView(callFrom: 'Login'),
+      page: () => OtpView(callFrom: 'Login'),
       binding: AuthBindings(),
     ),
     GetPage(
       name: RouteName.registerOTP,
-      page: () => OtpScreenView(callFrom: 'Register'),
+      page: () => OtpView(callFrom: 'Register'),
       binding: AuthBindings(),
     ),
     GetPage(
@@ -159,27 +159,27 @@ class AppRoutes {
     ),
     GetPage(
       name: RouteName.editProfile,
-      page: () => EditProfileScreen(
+      page: () => ProfileView(
         callFrom: 'Edit',
       ),
       // binding: ,
     ),
     GetPage(
-      name: RouteName.filterScreen, page: () => FilterScreen(),
+      name: RouteName.filterScreen, page: () => FilterView(),
       // binding: ,
     ),
     GetPage(
       name: RouteName.filterResultScreen,
-      page: () => const FilterResultScreen(),
+      page: () => const FilterResultView(),
       // binding: ,
     ),
     GetPage(
-      name: RouteName.paymentMethod, page: () => const PaymentMethodScreen(),
+      name: RouteName.paymentMethod, page: () => const PaymentMethodView(),
       // binding: ,
     ),
     GetPage(
       name: RouteName.restaurantDetails,
-      page: () => const RestaurantDetailScreen(),
+      page: () => const RestaurantDetailView(),
       // binding: ,
     ),
     GetPage(
