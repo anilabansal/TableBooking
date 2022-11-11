7aab8700cdae1c87cfba940013b7a9daee68d167
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-11-11 11:28:33 +0530

    merged

--- lib/controller/authentication/login_controller.dart
+++ lib/controller/authentication/login_controller.dart
@@ -6,7 +6,8 @@ import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   var mobileNumber = TextEditingController();
-
+  var countryCode = '1'.obs;
+  var countryFlag = '🇺🇸'.obs;
   ApiCalls apiCall = ApiCalls();
 
   Future<bool> loginUser({Map<String, String>? data}) async {
@@ -17,7 +18,19 @@ class LoginController extends GetxController {
     );
     print(data);
     print('Login Response ======> ${response.body}');
+    // if (response.body['response'] == 1 && response.body['data'] != null) {
+    //   return true;
+    // } else if (response.body['response'] == 1 &&
+    //     response.body['address'] == null) {
+    //   ShowToast.show(
+    //     msg: 'User not Registered!!',
+    //     isError: true,
+    //   );
+    // }
     if (response.body['response'] == 1) {
+      ShowToast.show(
+        msg: response.body['errorMessage'],
+      );
       return true;
     } else {
       ShowToast.show(
