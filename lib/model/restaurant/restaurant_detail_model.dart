d502cf37e585d0f899c11775e98e19bf1616fe33
Sahil Kaushal <SahilKaushal@parastechnologies>
2022-11-16 18:38:09 +0530

    Worked on Restaurant List API (Completed) Worked on Restaurant Details API (Completed) Worked on Restaurant About Us API (Completed) Worked on Restaurant Menu API (Completed) Worked on Restaurant Gallery API (Completed) Worked on Restaurant Reviews API (Completed)

---
+++ lib/model/restaurant/restaurant_detail_model.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'restaurantlist.dart';
+
+class RestaurantDetailModel {
+  int? totalCount;
+  List<Restaurantlist>? restaurantlist;
+  int? response;
+  String? errorMessage;
+  dynamic data;
+  dynamic token;
+
+  RestaurantDetailModel({
+    this.totalCount,
+    this.restaurantlist,
+    this.response,
+    this.errorMessage,
+    this.data,
+    this.token,
+  });
+
+  @override
+  String toString() {
+    return 'RestaurantDetailModel(totalCount: $totalCount, restaurantlist: $restaurantlist, response: $response, errorMessage: $errorMessage, data: $data, token: $token)';
+  }
+
+  factory RestaurantDetailModel.fromMap(Map<String, dynamic> data) {
+    return RestaurantDetailModel(
+      totalCount: data['totalCount'] as int?,
+      restaurantlist: (data['restaurantlist'] as List<dynamic>?)
+          ?.map((e) => Restaurantlist.fromMap(e as Map<String, dynamic>))
+          .toList(),
+      response: data['response'] as int?,
+      errorMessage: data['errorMessage'] as String?,
+      data: data['data'] as dynamic,
+      token: data['token'] as dynamic,
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+        'totalCount': totalCount,
+        'restaurantlist': restaurantlist?.map((e) => e.toMap()).toList(),
+        'response': response,
+        'errorMessage': errorMessage,
+        'data': data,
+        'token': token,
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [RestaurantDetailModel].
+  factory RestaurantDetailModel.fromJson(String data) {
+    return RestaurantDetailModel.fromMap(
+        json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [RestaurantDetailModel] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  RestaurantDetailModel copyWith({
+    int? totalCount,
+    List<Restaurantlist>? restaurantlist,
+    int? response,
+    String? errorMessage,
+    dynamic data,
+    dynamic token,
+  }) {
+    return RestaurantDetailModel(
+      totalCount: totalCount ?? this.totalCount,
+      restaurantlist: restaurantlist ?? this.restaurantlist,
+      response: response ?? this.response,
+      errorMessage: errorMessage ?? this.errorMessage,
+      data: data ?? this.data,
+      token: token ?? this.token,
+    );
+  }
+}
---
+++ lib/model/restaurant_about_us/restaurant_about_us.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'datum.dart';
+
+class RestaurantAboutUs {
+  int? response;
+  String? errorMessage;
+  List<Datum>? data;
+  dynamic token;
+
+  RestaurantAboutUs({
+    this.response,
+    this.errorMessage,
+    this.data,
+    this.token,
+  });
+
+  @override
+  String toString() {
+    return 'RestaurantAboutUs(response: $response, errorMessage: $errorMessage, data: $data, token: $token)';
+  }
+
+  factory RestaurantAboutUs.fromMap(Map<String, dynamic> data) {
+    return RestaurantAboutUs(
+      response: data['response'] as int?,
+      errorMessage: data['errorMessage'] as String?,
+      data: (data['data'] as List<dynamic>?)
+          ?.map((e) => Datum.fromMap(e as Map<String, dynamic>))
+          .toList(),
+      token: data['token'] as dynamic,
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+        'response': response,
+        'errorMessage': errorMessage,
+        'data': data?.map((e) => e.toMap()).toList(),
+        'token': token,
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [RestaurantAboutUs].
+  factory RestaurantAboutUs.fromJson(String data) {
+    return RestaurantAboutUs.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [RestaurantAboutUs] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  RestaurantAboutUs copyWith({
+    int? response,
+    String? errorMessage,
+    List<Datum>? data,
+    dynamic token,
+  }) {
+    return RestaurantAboutUs(
+      response: response ?? this.response,
+      errorMessage: errorMessage ?? this.errorMessage,
+      data: data ?? this.data,
+      token: token ?? this.token,
+    );
+  }
+}
